b2fc4dd75a0101ff6b6684cd970f72cd5f7c5133
arlsurya <suryaaryal204@gmail.com>
2023-07-14 22:56:33 +0545

    morgan package added for log the api request

--- server/app.js
+++ server/app.js
@@ -4,9 +4,12 @@ const dotenv = require('./env')
 const DB = require('./Core/db')
 const indexRoutes = require('./Routes/index')
 const apiRoutes = require('./Routes/api')
+const logger = require('morgan')
 DB()
 
+
 app.use(express.json())
+app.use(logger('dev'))
 
 app.use('/',indexRoutes)
 app.use('/api',apiRoutes)
