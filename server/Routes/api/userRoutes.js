3360df3b5bf620cef8599437a37435778319ea05
arlsurya <suryaaryal204@gmail.com>
2023-07-15 08:46:46 +0545

    is authenticated middleware created to check header token is valid or not

--- server/Routes/api/userRoutes.js
+++ server/Routes/api/userRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
+const isAuth = require('../../Middleware/isAuth')
 const userController = require('../../Controllers/userController')
 
 router.get('/', (req,res)=>{
     res.send('user routes')
 })
 
-router.post('/register', userController.register)
+router.post('/register',isAuth, userController.register)
 router.post('/login', userController.login)
 
 
