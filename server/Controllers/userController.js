503f1185e9257dd3e7be3cf5453cd897f75f9bd3
arlsurya <suryaaryal204@gmail.com>
2023-07-15 08:01:46 +0545

    user registration backend validation

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,7 +1,38 @@
+const userModel = require('../Models/userModel')
 module.exports = {
     register: async(req,res)=>{
+        console.log(req.body)
         try {
-            res.send('user register')
+        let {fullName, email, mobile, password } = req.body;
+        if(!fullName){
+            return res.status(400).json({
+                statusCode: 400,
+                Code: 0,
+                message: "full name is required"
+            })
+        }
+        if(!email){
+            return res.status(400).json({
+                statusCode: 400,
+                Code: 0,
+                message: "email is required"
+            })
+        }
+        if(!mobile){
+            return res.status(400).json({
+                statusCode: 400,
+                Code: 0,
+                message: "mobile is required"
+            })
+        }
+        if(!password){
+            return res.status(400).json({
+                statusCode: 400,
+                Code: 0,
+                message: "password is required"
+            })
+        }
+            
             
         } catch (error) {
             console.log(error)
