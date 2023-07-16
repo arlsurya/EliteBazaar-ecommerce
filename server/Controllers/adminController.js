af731e29d205f38a69dbb3155b053bd21a2a8fa3
arlsurya <suryaaryal204@gmail.com>
2023-07-16 20:57:29 +0545

    admin registration

--- server/Controllers/adminController.js
+++ server/Controllers/adminController.js
@@ -1,3 +1,10 @@
+const bcrypt = require('bcrypt')
+const saltRound = 10;
+const jwt = require('jsonwebtoken')
+const adminModel = require('../Models/adminModel')
+const Constants = require('../Constants')
+const Utilities = require('../Utilities')
+
 module.exports = {
     login: async(req,res)=>{
         try {
@@ -6,6 +13,88 @@ module.exports = {
         } catch (error) {
             console.log(error)
             
+        }
+    },
+    register: async (req, res) => {
+        console.log(req.body)
+        try {
+            let { fullName, email, mobile, password } = req.body;
+            if (!fullName) {
+                return res.status(400).json({
+                    statusCode: 400,
+                    Code: 0,
+                    message: "full name is required"
+                })
+            }
+            if (!email) {
+                return res.status(400).json({
+                    statusCode: 400,
+                    Code: 0,
+                    message: "email is required"
+                })
+            }
+            if (!mobile) {
+                return res.status(400).json({
+                    statusCode: 400,
+                    Code: 0,
+                    message: "mobile is required"
+                })
+            }
+            if (!password) {
+                return res.status(400).json({
+                    statusCode: 400,
+                    Code: 0,
+                    message: "password is required"
+                })
+            }
+
+            let existEmail = await adminModel.findOne({ email: email })
+            if (existEmail) {
+                return res.status(400).json({
+                    statusCode: 400,
+                    Code: 0,
+                    message: "email is registered with another account"
+                })
+            }
+
+            let existMobile = await adminModel.findOne({ mobile: mobile })
+            if (existMobile) {
+                return res.status(400).json({
+                    statusCode: 400,
+                    Code: 0,
+                    message: "mobile is registered with another account"
+                })
+            }
+
+            let hashedPassword = await bcrypt.hash(req.body.password, saltRound)
+
+            req.body.password = hashedPassword;
+
+            let admin = new adminModel(req.body)
+            admin = await admin.save()
+
+            let payload = {
+                id: admin._id
+            }
+
+            const authToken = jwt.sign(payload, Constants.jwtSectet, {
+                expiresIn: '7days'
+            })
+
+            return res.status(200).json({
+                statusCode: 200,
+                Code: 1,
+                token: `Bearer ${authToken}`,
+                message: 'admin registration sucessfully'
+            })
+
+        } catch (error) {
+            console.log(error)
+            return res.status(400).json({
+                statusCode: 400,
+                message: "something went wrong"
+            })
+
         }
     }
 }
--- server/Routes/api/adminRoutes.js
+++ server/Routes/api/adminRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
+const adminController = require('../../Controllers/adminController')
 
 router.get('/', (req,res)=>{
     res.send('admin routes')
-})
+}),
+
+router.post('/register',adminController.register)
 
 module.exports = router;
