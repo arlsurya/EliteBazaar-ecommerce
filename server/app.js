1b6d8cc6486a325d8d42a7c0745ee0a0c90e7e9b
arlsurya <suryaaryal204@gmail.com>
2023-07-28 20:26:52 +0545

    api rate limit set to 100 in 15mins

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const dotenv = require('./env')
 const DB = require('./Core/db')
 const indexRoutes = require('./Routes/index')
 const apiRoutes = require('./Routes/api')
+const rateLimit = require('express-rate-limit')
 const logger = require('morgan')
 const fileUpload = require('express-fileupload')
 app.use(fileUpload())
@@ -12,8 +13,16 @@ DB()
 app.use(cors())
 
 
+
 app.use(express.json())
 app.use(logger('dev'))
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max:100,
+
+})
+app.use(limiter)
 
 app.use('/',indexRoutes)
 app.use('/api',apiRoutes)
