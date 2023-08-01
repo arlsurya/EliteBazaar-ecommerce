83a168a5344ea1514a7e6d088aa3f77bd6c4f1ee
arlsurya <suryaaryal204@gmail.com>
2023-08-01 07:28:39 +0545

    admin component removed

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Header from '../AdminComponents/Header'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,9 +8,10 @@ export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+  
     >
-     
-    <Header/>
+      <h1>Landing page</h1>
+
   
     </main>
   )
