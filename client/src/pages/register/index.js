41e15b1b8f3e02a714839eb48d48d84fab71c09b
arlsurya <suryaaryal204@gmail.com>
2023-08-01 08:04:31 +0545

    formik and yup installed and validate the registration form

---
+++ client/src/pages/register/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+});
+
+export const registrationValidation = () => (
+  <div>
+    <h1>Signup</h1>
+    <Formik
+      initialValues={{
+        firstName: 'dd',
+        lastName: '',
+        email: '',
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={values => {
+        // same shape as initial values
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Field placeholder="first name" name="firstName" />
+          {errors.firstName && touched.firstName ? (
+            <div>{errors.firstName}</div>
+          ) : null}
+          <Field placeholder="last name" name="lastName" />
+          {errors.lastName && touched.lastName ? (
+            <div>{errors.lastName}</div>
+          ) : null}
+          <Field placeholder="email" name="email" type="email" />
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+
+export default registrationValidation
