9c5c02e912f1a270f23e0d332377666b487dddf6
arlsurya <suryaaryal204@gmail.com>
2023-08-01 18:58:18 +0545

    admin registration with formik validation

--- client/src/pages/admin/register/index.js
+++ client/src/pages/admin/register/index.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-function register() {
-  return (
-    <div>admin register</div>
-  )
-}
+const SignupSchema = Yup.object().shape({
+    fullName: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    mobile: Yup.number()
+        .typeError("That doesn't look like a phone number")
+        .positive("A phone number can't start with a minus")
+        .integer("A phone number can't include a decimal point")
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    password: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+});
 
-export default register
+export const registerAdmin = () => (
+    <div>
+        <h1>Admin Register</h1>
+
+        <Formik
+            initialValues={{
+                fullName: '',
+                email: '',
+                mobile: '',
+                password: ''
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={values => {
+                // same shape as initial values
+                    <Field placeholder="email" name="email" type="email" />
+                    {errors.email && touched.email ? <div>{errors.email}</div> : null}
+
+                    <Field placeholder="password" name="password" />
+                    {errors.password && touched.password ? (
+                        <div>{errors.password}</div>
+                    ) : null}
+           
+                console.log(values);
+            }}
+        >
+            {({ errors, touched }) => (
+                <Form>
+                    <Field placeholder="full name" name="fullName" />
+                    {errors.fullName && touched.fullName ? (
+                        <div>{errors.fullName}</div>
+                    ) : null}
+                    <Field placeholder="mobile" name="mobile" />
+                    {errors.mobile && touched.mobile ? (
+                        <div>{errors.mobile}</div>
+                    ) : null}         <button type="submit">Submit</button>
+                </Form>
+            )}
+        </Formik>
+        
+    </div>
+);
+
+export default registerAdmin;
