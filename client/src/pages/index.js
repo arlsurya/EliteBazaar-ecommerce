534077d1e6fafacecc5e1acf012ae2a1c79454da
arlsurya <suryaaryal204@gmail.com>
2023-08-06 08:41:15 +0545

    redux folder setup

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,29 +2,23 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useState, useEffect } from 'react'
 
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const [age,setAge]= useState('')
-  const [name,setName]= useState('')
-
+  const [age, setAge] = useState('')
+  const [name, setName] = useState('')
 
 
   return (
     <main
-      className={``}
-  
-    >
-      <h1 className='bg-red-400'>Landing page</h1>
-      <div className='main'>
-        <div className='navbar'>
+      className={``}>
+
+      <h1>test</h1>
+    
+
         
-        </div>
 
-      </div>
 
-  
     </main>
   )
 }
