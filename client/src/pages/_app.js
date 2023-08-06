327fe1f87b4f3616df3426d18586b7504890c716
arlsurya <suryaaryal204@gmail.com>
2023-08-06 08:52:15 +0545

    material component added

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,6 +1,4 @@
 // import '@/styles/globals.css'
-import '../pages/admin/home/home.css'
-
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,20 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useState, useEffect } from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,12 +22,68 @@ export default function Home() {
   const [age, setAge] = useState('')
   const [name, setName] = useState('')
 
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
+  );
+
 
   return (
     <main
       className={``}>
 
-      <h1>test</h1>
+<Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    
+    <h1>full width image</h1>
+
+
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <div>
+      <Button variant="contained" color="success">
+        Add Product
+      </Button>
+      </div>
+     
+    </Card>
+
     
 
         
