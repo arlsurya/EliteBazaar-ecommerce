feeab46532ae51a2d90992737ab60051481b1e70
arlsurya <suryaaryal204@gmail.com>
2023-09-16 20:29:41 +0545

    checkout section added

--- client/src/pages/[id].js
+++ client/src/pages/[id].js
@@ -1,5 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { LiaPhoenixSquadron } from "react-icons/lia";
+import { VscAccount } from "react-icons/vsc";
+
+
+
+
+
+
+
+
 
 // api url
 const apiURL = process.env.API_BASE_URL
@@ -25,6 +45,28 @@ function productById() {
 
   }, [])
 
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    console.log(categories)
+    setAnchorEl(event.currentTarget);
+  };
+
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
   const getProduct = async () => {
     try {
       const response = await fetch(`${apiURL}/api/user/product/${pid}`, {
@@ -49,6 +91,65 @@ function productById() {
 
 
   }
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 500 }}
+      role="presentation"
+    // onClick={toggleDrawer(anchor, false)}
+    // onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        <ListItem disablePadding>
+          <div
+
+          >
+            <div className="h-full overflow-y-auto p-4">
+              <h2 className="text-2xl font-semibold mb-4">Checkout</h2>
+
+              <div className="flex items-center justify-between mb-4">
+                <div className="w-1/4">
+                  <img
+                    src="your-image-url.jpg"
+                    alt="Product"
+                    className="w-full h-auto"
+                  />
+                </div>
+                <div className="w-1/4">
+
+                  <button className="bg-gray-200 p-2">-</button>
+                  <span className="mx-2">1</span>
+                  <button className="bg-gray-200 p-2">+</button>
+                </div>
+                <div className="w-1/4 text-right">
+                  $19.99
+                </div>
+              </div>
+
+              <div className='flex items-center justify-between mb-4'>
+                Total <br/> 
+                Shipping
+
+              </div>
+
+              <div className="mt-8">
+                <button className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </div>
+
+
+
+
+        </ListItem>
+
+      </List>
+
+
+    </Box>
+  );
+
 
   return (
     <div className="container mx-auto p-4">
@@ -78,15 +179,73 @@ function productById() {
               <p className="text-2xl text-green-600 font-semibold mb-4">
                 {item.productPrice}
               </p>
-              <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
-                Buy Now
-              </button>
+
+              <Toolbar>
+                {['right'].map((anchor) => (
+
+                  <React.Fragment key={anchor}>
+                    <button onClick={toggleDrawer(anchor, true)} className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+                      Buy Now
+                    </button>
+
+
+                    <Drawer
+                      anchor={anchor}
+                      open={state[anchor]}
+                      onClose={toggleDrawer(anchor, false)}
+                    >
+
+                      {list(anchor)}
+                    </Drawer>
+                  </React.Fragment>
+                ))}
+
+
+
+
+
+
+              </Toolbar>
             </div>
           </div>
         ))
       ) : (
         <p>Loading...</p>
       )}
+      <Toolbar>
+        {['right'].map((anchor) => (
+
+          <React.Fragment key={anchor}>
+
+            {/* <IconButton
+             
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton> */}
+
+
+            <Drawer
+              anchor={anchor}
+              open={state[anchor]}
+              onClose={toggleDrawer(anchor, false)}
+            >
+
+              {list(anchor)}
+            </Drawer>
+          </React.Fragment>
+        ))}
+
+
+
+
+
+
+      </Toolbar>
     </div>
   );
 
